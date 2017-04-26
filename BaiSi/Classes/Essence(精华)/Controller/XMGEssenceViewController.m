b2f03507f3f725dbca414f60764bba22f77374a9
Yang <yangyuren@YangdeMacBook-Pro.local>
2017-04-26 14:12:22 +0800

    modify

--- BaiSi/Classes/Essence(精华)/Controller/XMGEssenceViewController.m
+++ BaiSi/Classes/Essence(精华)/Controller/XMGEssenceViewController.m
@@ -7,9 +7,11 @@
 //
 
 #import "XMGEssenceViewController.h"
-
+#import "XMGButton.h"
 @interface XMGEssenceViewController ()
-
+@property(nonatomic,weak)UIScrollView * scroll;
+@property(nonatomic,weak)UIView * titleView;
+@property(nonatomic,weak)XMGButton * preButton;
 @end
 
 @implementation XMGEssenceViewController
@@ -25,7 +27,7 @@
 //scroll的设置
 -(void)setupScrollView{
     UIScrollView * scroll = [[UIScrollView alloc] init];
-    scroll.backgroundColor = [UIColor greenColor];
+    scroll.backgroundColor = [UIColor blueColor];
     scroll.frame = self.view.frame;
     [self.view addSubview:scroll];
 
@@ -36,6 +38,41 @@
     titleView.backgroundColor = [UIColor colorWithRed:1.0 green:1.0 blue:1.0 alpha:0.5];
     titleView.frame = CGRectMake(0, 64,XMGScreenW , 35);
     [self.view addSubview:titleView];
+    self.titleView =  titleView;
+    //加载标题按钮
+    [self setupTitleButtons];
+    //加载标题下划线
+    [self setupTitleUnderLine];
+}
+-(void)setupTitleButtons{
+    CGFloat btnX = 0;
+    CGFloat btnY = 0;
+    CGFloat btnH = self.titleView.xmg_height;
+    CGFloat btnW = self.titleView.xmg_width/5;
+    NSArray * arr = @[@"全部",@"视频",@"声音",@"图片",@"文字"];
+    for(NSInteger i=0;i<5;i++){
+        XMGButton * titleButton = [[XMGButton alloc] init];
+        btnX = btnW * i;
+        titleButton.frame = CGRectMake(btnX, btnY, btnW, btnH);
+        [titleButton setTitle:arr[i] forState:UIControlStateNormal];
+        [self.titleView addSubview:titleButton];
+        [titleButton addTarget:self action:@selector(titleBtnClick:) forControlEvents:UIControlEventTouchUpInside];
+        [titleButton setTitleColor:[UIColor darkGrayColor] forState:UIControlStateNormal];
+        [titleButton setTitleColor:[UIColor redColor] forState:UIControlStateSelected];
+    }
+    
+}
+//点击按钮事件
+-(void)titleBtnClick:(XMGButton *)btn{
+    //上一个按钮恢复颜色
+    self.preButton.selected = NO;
+    btn.selected = YES;
+    //重新赋值上一个按钮
+    self.preButton = btn;
+}
+//设置按钮下划线
+-(void)setupTitleUnderLine{
+    
 }
 
 -(void)setupNavBar{
---
+++ BaiSi/Classes/Essence(精华)/View/XMGButton.m
@@ -0,0 +1,17 @@
+//
+//  XMGButton.m
+//  BaiSi
+//
+//  Created by Yang on 2017/4/26.
+//  Copyright © 2017年 Yang. All rights reserved.
+//
+
+#import "XMGButton.h"
+
+@implementation XMGButton
+
+//重写该方法,用户无法进入高亮该状态
+-(void)setHighlighted:(BOOL)highlighted{
+   
+}
+@end
